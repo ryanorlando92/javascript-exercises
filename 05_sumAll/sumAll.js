e0c626a9f663279feb52eb7ed8eeea42f49d3f2a
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-22 23:24:48 -0400

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if ((typeof startNum != 'number') || (typeof endNum != 'number')) return "ERROR";
+    if (startNum < 0 || endNum < 0) return "ERROR";
+    
+    let sum = 0;
+    if (startNum > endNum) {
+        for (i = endNum; i <= startNum; i++) {
+            sum += i;
+        }
+    }
 
+    for (i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
