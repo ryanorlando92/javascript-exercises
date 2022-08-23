282ae20b933c566410cf8f760dab682755f5faa9
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-22 23:52:31 -0400

    solve 06: feels good man

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ( year.toString().slice(-2) == '00') {
+        return !(year % 400);
+    }
+    return !(year % 4) 
 };
 
 // Do not edit below this line
