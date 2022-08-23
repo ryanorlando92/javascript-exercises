382fee5e5cb5b6584956224ebe49699992f538f0
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-22 21:05:05 -0400

    complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let string1 = '';
+    if (num < 0) return 'ERROR';
+    for (let i = 0; i < num; i++) {
+        string1 += string;
+    }
+    return string1;
 };
 
 // Do not edit below this line
