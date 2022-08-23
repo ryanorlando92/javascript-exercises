b872f0804549c22463db508cac304d32b6182f12
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-22 22:50:59 -0400

    exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    for (i = 0; i < arr.length; i++) {
 
+        let a = 0;
+        while (a < theArgs.length) {
+
+            if (arr[i] === theArgs[a]) {
+                arr.splice(i, 1);
+                i = -1;
+            }
+
+        a++;
+        }    
+    }
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*
+
+...theArgs are stored as array too. so have to compare each element to to each
+*/
