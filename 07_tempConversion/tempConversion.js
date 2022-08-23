f598b6aa4fdf9a2842938f2d9b46168ff3337708
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-23 00:07:02 -0400

    finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  const c = ( (f - 32) / 1.8 );
+  return Math.round( (c + Number.EPSILON) * 10 ) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  const f = ( (c * 1.8) + 32 );
+  return Math.round( (f + Number.EPSILON) * 10 ) / 10;
 };
 
 // Do not edit below this line
