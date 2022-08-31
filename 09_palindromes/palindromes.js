b28872efabe813c20e1b87beced98e51c28f93f7
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-30 22:14:15 -0400

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const noPunctuation = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    const noSpaces = noPunctuation.replace(/\s{1,}/g,"");
+    const finalString = noSpaces.toUpperCase();
+
+    let output = '';
+    for (let i = (finalString.length - 1); i >= 0; i--) {
+        output += finalString[i];
+    }
+
+    return (output === finalString);
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
