037de2fc740094065e040f9b3ccb20c6e5544e25
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-30 21:59:01 -0400

    complete06

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,72 @@
-const add = function() {
-	
+const add = function(...theArgs) {
+	let total = 0;
+  for (const arg of theArgs) {
+    total += Number(arg);
+  }
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...theArgs) {
+  let total = null;
+	for (const arg of theArgs) {
+    if (total === null) {
+      total = arg;
+      continue;
+    }
+    total -= Number(arg);
+  }
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (const index of array) {
+    total += index;
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(...theArgs) {
+  let total = 1;
+	for (const arg of theArgs) {
 
+    if (Array.isArray(arg)) {
+      for (const index of arg) {
+        total *= index;
+      }
+      continue;
+    }
+
+    total *= Number(arg);
+
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base,power) {
+  let total = null;
+	for (let i = 0; i < power; i++) {
+    if (total === null) {
+      total = base;
+      continue;
+    }
+
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let total = 1;
+  for (let i = num; i > 0; i--) {
+    if (i === num) {
+      total = num;
+      continue;
+    }
+    
+    total *= i;
+  }
+  return total;
 	
 };
 
