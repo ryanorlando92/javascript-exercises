af29dee2fb0cdaf261994afa0c91a1c23c7b6263
ryanorlando92 <ryan.orlando92@gmail.com>
2022-09-01 14:13:48 -0400

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const output = array.reduce((oldest, nextPerson) => {
+        const a = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const b = getAge(nextPerson.yearOfBirth, nextPerson.yearOfDeath);
+        return (a > b) ? oldest : nextPerson;
+        });
+    return output;
+}
 
-};
+
+//getAge function so we dont destroy the original data
+function getAge(birth, death) {
+if (!death) {
+    death = new Date().getFullYear();
+}
+return death - birth;
+}
+   
+      
+   
+   // this code adds a deathyear to the original data, altering it. this wont work
+    /*  const oldest = array.sort( (a, b) => {
+        const currentYear = new Date().getFullYear();
+        if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+            a.yearOfDeath = currentYear;
+            b.yearOfDeath = currentYear;
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+       
+        return (lastGuy > nextGuy) ? -1 : 1;
+    });
+    return oldest[0]; */
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
